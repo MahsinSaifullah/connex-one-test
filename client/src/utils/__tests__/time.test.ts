6947c1d37e070e41a40df479258148797a5e593f
macbookpro <mahsinsaifullah@gmail.com>
2023-03-19 10:48:49 +0000

    test: add unit tests for time util functions

---
+++ client/src/utils/__tests__/time.test.ts
@@ -0,0 +1,34 @@
+import {
+  calculateTimeDifference,
+  convertSecondToHourMinSec,
+  formatTimeDisplay,
+} from '../time.utils';
+
+describe('calculateTimeDifference', () => {
+  it('should return the correct value', () => {
+    jest.spyOn(Math, 'round').mockReturnValue(100);
+    expect(calculateTimeDifference(10)).toBe(90);
+  });
+});
+
+describe('convertSecondToHourMinSec', () => {
+  it('should return the correct value', () => {
+    expect(convertSecondToHourMinSec(1000)).toMatchObject({
+      hours: 0,
+      minutes: 16,
+      seconds: 40,
+    });
+  });
+});
+
+describe('formatTimeDisplay', () => {
+  it('should return the correct value', () => {
+    expect(
+      formatTimeDisplay({
+        hours: 0,
+        minutes: 16,
+        seconds: 40,
+      })
+    ).toMatch('0:16:40');
+  });
+});
